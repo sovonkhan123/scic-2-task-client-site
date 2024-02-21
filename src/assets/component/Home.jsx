24c7d8a9e6092647764053c43be6bf3cc8f9c1b3
Azmiruzzaman khan sovon <sovonkhan77@gmail.com>
2024-02-22 00:01:48 +0600

    new arrival

--- src/assets/component/Home.jsx
+++ src/assets/component/Home.jsx
@@ -1,3 +1,4 @@
+import OurProduct from "./OurProduct";
 import Slider from "./Slider";
 
 
@@ -5,6 +6,7 @@ const Home = () => {
     return (
         <div>
             <Slider></Slider>
+            <OurProduct></OurProduct>
             
         </div>
     );
---
+++ src/assets/component/OurProduct.jsx
@@ -0,0 +1,18 @@
+
+
+const OurProduct = () => {
+    return (
+        <div className="my-8">
+            <h2 className="uppercase text-center text-red-700 font-bold text-lg"> Our Product</h2>
+            <p className="text-center my-2 ">Inspirational designs, illustrations, and graphic elements from the world’s best designers.</p>
+
+            <div>
+                <img src="https://cdn.dribbble.com/users/4324441/screenshots/18363223/media/1218b19519b4b2a92dd03a102683295a.png" alt="" />
+                <img src="https://cdn.dribbble.com/userupload/10754689/file/original-8de6130be37248dc5670b9da1350c3c0.png?resize=320x240&vertical=center" alt="" /><img src="" alt="" /><img src="" alt="" /><img src="" alt="" /><img src="" alt="" /><img src="" alt="" /><img src="" alt="" /><img src="" alt="" /><img src="" alt="" /><img src="" alt="" /><img src="" alt="" />
+            </div>
+
+        </div>
+    );
+};
+
+export default OurProduct;
