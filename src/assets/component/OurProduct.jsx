4bf950513870684e5d73779357741d39cd3d956a
Azmiruzzaman khan sovon <sovonkhan77@gmail.com>
2024-02-22 00:25:15 +0600

    all photo are added

--- src/assets/component/OurProduct.jsx
+++ src/assets/component/OurProduct.jsx
@@ -3,12 +3,26 @@
 const OurProduct = () => {
     return (
         <div className="my-8">
-            <h2 className="uppercase text-center text-red-700 font-bold text-lg"> Our Product</h2>
-            <p className="text-center my-2 ">Inspirational designs, illustrations, and graphic elements from the world’s best designers.</p>
+            <h2 className="uppercase text-center text-red-700 font-bold text-lg"> new arrival</h2>
+            <p className="text-center ">Inspirational designs, illustrations, and graphic elements from the world’s best designers.</p>
+            <img className="mt-7" src="https://cdn.dribbble.com/users/4324441/screenshots/18363223/media/1218b19519b4b2a92dd03a102683295a.png" alt="" />
 
-            <div>
-                <img src="https://cdn.dribbble.com/users/4324441/screenshots/18363223/media/1218b19519b4b2a92dd03a102683295a.png" alt="" />
-                <img src="https://cdn.dribbble.com/userupload/10754689/file/original-8de6130be37248dc5670b9da1350c3c0.png?resize=320x240&vertical=center" alt="" /><img src="" alt="" /><img src="" alt="" /><img src="" alt="" /><img src="" alt="" /><img src="" alt="" /><img src="" alt="" /><img src="" alt="" /><img src="" alt="" /><img src="" alt="" /><img src="" alt="" />
+            <h2 className="uppercase text-center text-lg font-bold mt-8">our products</h2>
+
+            <div className="grid gap-4 rounded-md mt-12 grid-cols-3">
+                
+                <img src="https://cdn.dribbble.com/userupload/10754689/file/original-8de6130be37248dc5670b9da1350c3c0.png?resize=320x240&vertical=center" alt="" />
+                <img src="https://cdn.dribbble.com/userupload/11553919/file/original-ef80dda0cb40fabe51d0cecacdb3e512.png?crop=1x1-961x721&resize=320x240&vertical=center" alt="" />
+                <img src="https://cdn.dribbble.com/userupload/11086732/file/original-b95f7eeb5aa56ed4a454bc3a4e5a837f.png?crop=0x0-2000x1500&resize=400x300&vertical=center" alt="" />
+                <img src="https://cdn.dribbble.com/userupload/3187292/file/original-f9aa2ac77689a64a96b4a3e00dcb211c.jpg?resize=400x300&vertical=center" alt="" />
+                <img src="https://cdn.dribbble.com/userupload/11041827/file/original-c81e920acf179c54c82e485ae7f6a81d.jpg?resize=320x240&vertical=center" alt="" />
+                <img src="https://cdn.dribbble.com/userupload/11800921/file/original-333a1cbf0c28aa8130b61adfd2df1a3d.png?resize=320x240&vertical=center" alt="" />
+                <img src="https://cdn.dribbble.com/userupload/4647502/file/original-d9c4706f56c7060de7e8fe9be528f332.png?resize=320x240&vertical=center" alt="" />
+                <img src="https://cdn.dribbble.com/users/4324441/screenshots/18364098/media/47b71fd2466f26d48c8638ac0ce3e9a6.png?resize=320x240&vertical=center" alt="" />
+                <img src="https://cdn.dribbble.com/users/1307817/screenshots/12844918/media/deef7a979a7af726e5dc31158fc714b5.png?resize=320x240&vertical=center" alt="" />
+                <img src="https://cdn.dribbble.com/userupload/9069101/file/still-f71ac11fc3bf57f03ffaf4790ecd33e1.png?resize=320x240&vertical=center" alt="" />
+                <img src="https://cdn.dribbble.com/userupload/13012542/file/original-afe8b44cbfa935052ac5fc00a9a00497.png?crop=0x0-1600x1200&resize=320x240&vertical=center" alt="" />
+                <img src="https://cdn.dribbble.com/userupload/5097353/file/original-08d52f08b7e4305ed9058843a1ed1ea8.jpg?resize=320x240&vertical=center" alt="" />
             </div>
 
         </div>
